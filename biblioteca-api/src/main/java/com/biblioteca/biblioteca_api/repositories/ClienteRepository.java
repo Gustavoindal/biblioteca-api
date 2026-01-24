f54096acb19b346ca82644485026d5c307999f35
Gustavoindal <indalenciogustavo@gmail.com>
2026-01-24 02:28:17 -0300

    Delete biblioteca-api directory

--- biblioteca-api/src/main/java/com/biblioteca/biblioteca_api/repositories/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.biblioteca.biblioteca_api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.biblioteca.biblioteca_api.models.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, String> {
-    
-}
--- biblioteca-api/src/main/java/com/biblioteca/biblioteca_api/repositories/LivroRepository.java
+++
@@ -1,8 +0,0 @@
-package com.biblioteca.biblioteca_api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.biblioteca.biblioteca_api.models.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, String> {
-    
-}
